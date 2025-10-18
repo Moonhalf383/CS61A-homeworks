b0f6bfae6291707c0f654f2c5b5869dfacaac4ff
Moonhalf383 <13776003846@163.com>
2025-10-18 14:39:35 +0800

    Data Abstraction Lecture completed.

---
+++ notes/data_abstraction.py
@@ -0,0 +1,46 @@
+'Rational arithmetic'
+
+def mul_rational(x,y):
+	return rational(numer(x)*numer(y),denom(x)*denom(y))
+
+def add_rational(x,y):
+	nx,dx = numer(x),denom(x)
+	ny,dy = numer(y),denom(y)
+	return rational(nx*dy+ny*dx,dx*dy)
+	
+def equal_rational(x,y):
+	return numer(x)*denom(y) == numer(y)*denom(x)
+
+def print_rational(x):
+    print(numer(x),'/',denom(x))
+
+'Constructor and selectors'
+
+def rational(n,d):
+    def select(name):
+        if name == 'n':
+            return n 
+        elif name == 'd':
+            return d 
+    return select
+
+'''
+def numer(x):
+	return x[0]
+	
+def denom(x):
+	return x[1]
+'''
+
+def numer(x):
+    return x('n')
+
+def denom(x):
+    return x('d')
+
+
+
+
+
+
+
